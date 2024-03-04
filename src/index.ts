b76f49ca15256a39fe9f114c51060d92dc5c543d
Rafael Rahal <rrrahal@gmail.com>
2024-03-04 13:34:49 +0000

    initial node, ts and express setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application } from "express";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.get("/", async(_req, res) => {
+  res.send({
+    message: "Hello World"
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
